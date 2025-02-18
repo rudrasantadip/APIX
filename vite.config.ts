0d6cb46cab789f8b19980947f3a2d2704ac55ea4
rudrasantadip <santadip9@gmail.com>
2025-02-18 14:52:43 +0530

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,26 @@
 //   },
 // });
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // If using React
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  resolve: {
+    alias: {
+      // Resolving `@` to the `src` directory
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    // This is optional, you can add custom settings for your dev server
+    port: 3000, // Or any port of your choice
+    open: true, // Automatically open the app in the browser
+  },
   build: {
-    outDir: "dist",
+    // You can add custom build configurations here if needed
+    outDir: 'dist', // Output directory for production build
   },
 });

a68fecde61140a61d829210b1f8a8042b9110a1c
rudrasantadip <santadip9@gmail.com>
2025-02-18 14:37:35 +0530

    updated viteconfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // If using React
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: "./",
+  build: {
+    outDir: "dist",
   },
 });
